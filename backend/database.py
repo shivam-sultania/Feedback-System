bc1c4af4b6ecdb35ba3a7989d668841b0f982f58
shivam sultania <shivamsultania7@gmail.com>
2025-09-06 16:42:42 +0530

    removed SQLite db and added new postgre sql

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,18 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
 # Use env variable if set, else fallback to local SQLite
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 )
+
+print(f"Using database: {DATABASE_URL}")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
